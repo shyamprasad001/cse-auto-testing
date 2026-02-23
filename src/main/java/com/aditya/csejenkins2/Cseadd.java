3cfa20314b0a5399a3625195b0deed8257258010
Shyam Mantri <iamshyam1911@gmail.com>
2026-02-23 11:58:51 +0530

    first commit

---
+++ src/main/java/com/aditya/csejenkins2/Cseadd.java
@@ -0,0 +1,12 @@
+package com.aditya.csejenkins2;
+
+public class Cseadd {
+	public int add(int a, int b){
+		return a + b;
+	}
+	
+	public int mul(int a, int b){
+		return a * b;
+	}
+	
+}
---
+++ src/test/java/com/aditya/csejenkins2/CseaddTest.java
@@ -0,0 +1,44 @@
+package com.aditya.csejenkins2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CseaddTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	Cseadd c = new Cseadd();
+	
+	
+	@Test
+	void csetest1() {
+		int result = c.add(2,3);
+		assertEquals(5, result);
+	}
+	
+	@Test
+	void csetest2() {
+		int result = c.mul(2,3);
+		assertEquals(6, result);
+	}
+
+}
